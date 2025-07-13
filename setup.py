f2c02a5fcbd6c8403b40bae038dfa5ce09b19168
rxl895 <143743393+rxl895@users.noreply.github.com>
2025-07-13 17:55:40 -0400

    Add setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='FAIRLinked',
-    version='0.2.0.4',
-    description='Transform research data into FAIR-compliant RDF using the RDF Data Cube Vocabulary. Align your datasets with MDS-Onto and convert them into Linked Data, enhancing interoperability and reusability for seamless data integration. See the README or vignette for more information. This tool is used by the SDLE Research Center at Case Western Reserve University.',
+    version='0.3.0.0',  # ⬅️ Updated version to match recent work
+    description='Transform research data into FAIR-compliant RDF using the RDF Data Cube Vocabulary.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Balashanmuga Priyan Rajamohan, Kai Zheng, Benjamin Pierce, Erika I. Barcelos, Roger H. French',
     author_email='rxf131@case.edu',
     license='BSD-2-Clause',
-    packages=find_packages(),
+    packages=find_packages(where='packages'),  # ⬅️ Ensure your code is under 'packages/'
+    package_dir={'': 'packages'},              # ⬅️ Map logical root to physical 'packages/'
     install_requires=[
         'rdflib>=7.0.0',
         'typing-extensions>=4.0.0',
@@ -28,5 +29,15 @@ setup(
             'FAIRLinked=FAIRLinked.__main__:main'
         ]
     },
+    include_package_data=True,
+    zip_safe=False,
     python_requires='>=3.9.18',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis'
+    ],
 )
